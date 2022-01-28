8920e3f6da8cbf6825def5927570d38f5aacde9e
asaduzzamanasik <asaduzzamanasik20@.com>
2022-01-28 15:01:47 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,26 @@
+var bauget = prompt("Enter Your amount");
+
+
+if(bauget > 5000){
+
+    console.log( "i will by the phone olton")
+}
+
+else if(bauget > 9000){
+
+    console.log( "i will by the phone samsang")
+}
+else if(bauget > 14000){
+
+    console.log( "i will by the phone saumi")
+}
+else if(bauget > 19000){
+
+    console.log( "i will by the phone i phone")
+}
+
+else{
+
+
+    console.log( "tomar phone kena lagbe na")
+}
